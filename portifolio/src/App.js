bdb1ac45229d8f459368a7e1b397d341a50ab96b
moses <moskopower01@gmail.com>
2026-02-16 11:37:06 +0300

    removed header

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+
 import Hero from './components/Hero';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -8,7 +8,7 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <Header />
+    
       <Hero />
       <About />
       <Contact />
